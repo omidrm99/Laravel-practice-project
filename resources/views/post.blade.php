c38f838a4349ec0a6ca12bf6f702f56b90c3795b
Omid RM99 <omidrm99@gmail.com>
2024-04-24 20:19:42 +0330

    a layout crated to manage blade view files and generate html

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,25 +1,14 @@
-<html lang="eng">
-<head>
+@extends ('layout')
 
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My Blog</title>
-</head>
-<body>
+@section ('content')
+<h1>
+    {{ $post->title; }}
+</h1>
 
-<article>
-    <h1>
-        {{ $post->title; }}
-    </h1>
-
-    <div>
-        <p>
-            {!! $post->body; !!}
-        </p>
-    </div>
-    <a href="/">Go Back</a>
-</body>
-</html>
+<div>
+    <p>
+        {!! $post->body; !!}
+    </p>
+</div>
+<a href="/">Go Back</a>
+@endsection
